28fd4bf5cbff43f954efe03f46a3384c43319d1b
Selin Gölpek <golpekselin@gmail.com>
2022-10-31 00:08:03 +0300

    fixed some bugs in basefaction.py and factions.py

--- BaseFaction.py
+++ BaseFaction.py
@@ -1,4 +1,64 @@
+class Merchant:
+    starting_wp = 10
+    starting_ap = 10
+
+    def __init__(self, start_wp=10, start_ap=10):
+        Merchant.starting_wp = start_wp
+        Merchant.starting_ap = start_ap
+        self.wp_left = start_wp
+        self.ap_left = start_ap
+        self.faction1: 'Faction' = None
+        self.faction2: 'Faction' = None
+        self.faction3: 'Faction' = None
+        self.revenue = 0
+
+    def assign_factions(self, faction1: 'Faction', faction2: 'Faction', faction3: 'Faction'):
+        self.faction1 = faction1
+        self.faction2 = faction2
+        self.faction3 = faction3
+
+    def sell_weapons(self, faction: 'Faction', ordered_wp):
+        flag = False
+        if faction not in [self.faction1, self.faction2, self.faction3]:
+            print("The faction you want to sell weapons is not in merchant's control")
+
+        elif not faction.is_alive:
+            print("“The faction you want to sell weapons is dead!")
+
+        elif ordered_wp > self.wp_left:
+            print("You try to sell more weapons than you have in possession.")
+
+        else:
+            self.wp_left -= ordered_wp
+            self.revenue += ordered_wp
+            print("Weapons sold!")
+            flag = True
+        return flag
+
+    def sell_armors(self, faction: 'Faction', ordered_ap):
+        flag = False
+        if faction not in [self.faction1, self.faction2, self.faction3]:
+            print("The faction you want to sell armors is not in merchant's control")
+        elif not faction.is_alive:
+            print("“The faction you want to sell armors is dead!")
+
+        elif ordered_ap > self.ap_left:
+            print("You try to sell more armors than you have in possession.")
+
+        else:
+            self.ap_left -= ordered_ap
+            self.revenue += ordered_ap
+            print("Armors sold!")
+            flag = True
+        return flag
+
+    def end_turn(self):
+        self.wp_left = Merchant.starting_wp
+        self.ap_left = Merchant.starting_ap
+
+
 class Faction:
+
     # init method
     def __init__(self, name="Default", no_units=50, attack_pt=30, health_pt=150, unit_rgn=10):
         self.name = name
@@ -22,17 +82,22 @@ class Faction:
 
     def receive_attack(self, attacker: 'Faction', no_att_units, att_pt):
         self.no_units -= no_att_units * att_pt / self.health_pt
-
-        if self.no_units == 0:
+        if self.no_units <= 0:
             self.is_alive = False
+            self.no_units = 0
+        self.total_health = self.no_units * self.health_pt
 
-    def purchase_weapons(self, weapon_pt, coeff_weapon_pt, weapon_price):
-        self.attack_pt += weapon_pt * coeff_weapon_pt
-        return weapon_pt * weapon_price
+    def purchase_weapons(self, merchant: 'Merchant', weapon_pt, coeff_weapon_pt, weapon_price):
+        if merchant.sell_weapons(self, weapon_pt):
+            self.attack_pt += weapon_pt * coeff_weapon_pt
+            return weapon_pt * weapon_price
+        return 0
 
-    def purchase_armors(self, armors_pt, coeff_armors_pt, armor_price):
-        self.health_pt += armors_pt * coeff_armors_pt
-        return armors_pt * armor_price
+    def purchase_armors(self, merchant: 'Merchant', armors_pt, coeff_armors_pt, armor_price):
+        if merchant.sell_armors(self, armors_pt):
+            self.health_pt += armors_pt * coeff_armors_pt
+            return armors_pt * armor_price
+        return 0
 
     def __str__(self):
         return f"""
@@ -42,5 +107,4 @@ class Faction:
                 Attack Point:\t{self.attack_pt}
                 Health Point:\t{self.health_pt}
                 Unit Regen Number:\t{self.unit_rgn}
-                Total Faction Health:\t{self.total_health}
-        """
+                Total Faction Health:\t{self.total_health}"""
--- Factions.py
+++ Factions.py
@@ -1,8 +1,11 @@
-from BaseFaction import Faction
+from BaseFaction import Faction, Merchant
+
+
 class Orcs(Faction):
     """ no __init__() at all here, use the parent's __init__ """
 
-    def perform_attack(self, no_att_units1=0, att_pt1=0, no_att_units2=0, att_pt2=0):
+    def perform_attack(self, no_att_units1=0, att_pt1=0, no_att_units2=0,
+                       att_pt2=0):  # for all factions, all are zero by default, so no attack if conditions aren't satisfied
         if self.enemy1.is_alive or self.enemy2.is_alive:
             if self.enemy1.is_alive:
                 no_att_units1 = self.no_units
@@ -13,8 +16,8 @@ class Orcs(Faction):
                 att_pt2 = self.attack_pt
 
             if self.enemy1.is_alive and self.enemy2.is_alive:
-                no_att_units1 *= self.no_units * (0.7 if isinstance(self.enemy1, Elves) else 0.3)
-                no_att_units2 *= self.no_units * (0.7 if isinstance(self.enemy2, Elves) else 0.3)
+                no_att_units1 = self.no_units * (0.7 if isinstance(self.enemy1, Elves) else 0.3)
+                no_att_units2 = self.no_units * (0.7 if isinstance(self.enemy2, Elves) else 0.3)
 
         super().perform_attack(no_att_units1, att_pt1, no_att_units2, att_pt2)
 
@@ -22,14 +25,14 @@ class Orcs(Faction):
         att_pt = att_pt * (0.75 if isinstance(attacker, Elves) else 0.80)
         super().receive_attack(attacker, no_att_units, att_pt)
 
-    def purchase_weapons(self, weapon_pt, coeff_weapon_pt, weapon_price):
-        super().purchase_weapons(weapon_pt, 2, 20)
+    def purchase_weapons(self, merchant: 'Merchant', weapon_pt):
+        super().purchase_weapons(merchant, weapon_pt, coeff_weapon_pt=2, weapon_price=20)
 
-    def purchase_armors(self, armors_pt, coeff_armors_pt, armor_price):
-        super().purchase_armors(armors_pt, 3, 1)
+    def purchase_armors(self, merchant: 'Merchant', armors_pt):
+        super().purchase_armors(merchant, armors_pt, coeff_armors_pt=3, armor_price=1)
 
     def __str__(self):
-        return '“Stop running, you’ll only die tired!"\n\n' + super().__str__()
+        return '“Stop running, you’ll only die tired!"\n' + super().__str__()
 
 
 #
@@ -49,20 +52,19 @@ class Dwarves(Faction):
 
             if self.enemy1.is_alive and self.enemy2.is_alive:
                 no_att_units1, no_att_units2 = self.no_units / 2
-
         super().perform_attack(no_att_units1, att_pt1, no_att_units2, att_pt2)
 
     def receive_attack(self, attacker: 'Faction', no_att_units, att_pt):
         super().receive_attack(attacker, no_att_units, att_pt)
 
-    def purchase_weapons(self, weapon_pt, coeff_weapon_pt, weapon_price):
-        super().purchase_weapons(weapon_pt, 1, 10)
+    def purchase_weapons(self, merchant: 'Merchant', weapon_pt):
+        super().purchase_weapons(merchant, weapon_pt, coeff_weapon_pt=1, weapon_price=10)
 
-    def purchase_armors(self, armors_pt, coeff_armors_pt, armor_price):
-        super().purchase_armors(armors_pt, 2, 3)
+    def purchase_armors(self, merchant: 'Merchant', armors_pt):
+        super().purchase_armors(merchant, armors_pt, coeff_armors_pt=2, armor_price=3)
 
     def __str__(self):
-        return '"Taste the power of our axes!" \n\n' + super().__str__()
+        return '"Taste the power of our axes!" \n' + super().__str__()
 
 
 class Elves(Faction):
@@ -80,8 +82,8 @@ class Elves(Faction):
                 att_pt2 = self.attack_pt * (1.50 if isinstance(self.enemy2, Dwarves) else 1)
 
             if self.enemy1.is_alive and self.enemy2.is_alive:
-                no_att_units1 *= self.no_units * (0.6 if isinstance(self.enemy1, Orcs) else 0.4)
-                no_att_units2 *= self.no_units * (0.6 if isinstance(self.enemy2, Orcs) else 0.4)
+                no_att_units1 = self.no_units * (0.6 if isinstance(self.enemy1, Orcs) else 0.4)
+                no_att_units2 = self.no_units * (0.6 if isinstance(self.enemy2, Orcs) else 0.4)
 
         super().perform_attack(no_att_units1, att_pt1, no_att_units2, att_pt2)
 
@@ -89,61 +91,11 @@ class Elves(Faction):
         att_pt = att_pt * (1.25 if isinstance(attacker, Orcs) else 0.75)
         super().receive_attack(attacker, no_att_units, att_pt)
 
-    def purchase_weapons(self, weapon_pt, coeff_weapon_pt, weapon_price):
-        super().purchase_weapons(weapon_pt, 2, 15)
+    def purchase_weapons(self, merchant: 'Merchant', weapon_pt):
+        super().purchase_weapons(merchant, weapon_pt, coeff_weapon_pt=2, weapon_price=15)
 
-    def purchase_armors(self, armors_pt, coeff_armors_pt, armor_price):
-        super().purchase_armors(armors_pt, 4, 5)
+    def purchase_armors(self, merchant: 'Merchant', armors_pt):
+        super().purchase_armors(merchant, armors_pt, coeff_armors_pt=4, armor_price=5)
 
     def __str__(self):
-        return '"You cannot reach our elegance." \n\n' + super().__str__()
-
-
-class Merchant:
-    starting_wp = 10
-    starting_ap = 10
-    def __init__(self, start_wp=10, start_ap=10):
-        Merchant.starting_wp = start_wp
-        Merchant.starting_ap = start_ap
-        self.wp_left = self.start_wp
-        self.ap_left = self.start_ap
-        self.faction1: 'Faction' = None
-        self.faction2: 'Faction' = None
-        self.faction3: 'Faction' = None
-        self.revenue = 0
-
-
-    def assign_factions(self, faction1: 'Faction', faction2: 'Faction', faction3: 'Faction'):
-        self.faction1 = faction1
-        self.faction2 = faction2
-        self.faction3 = faction3
-
-    def sell_weapons(self, faction: 'Faction', ordered_wp):
-        if not faction.is_alive:
-            print("“The faction you want to sell weapons is dead!")
-            return False
-
-        if ordered_wp > self.wp_left:
-            print("You try to sell more weapons than you have in possession.")
-            return False
-
-        self.wp_left -= ordered_wp
-        print("Weapons sold!")
-        return True
-
-    def sell_armors(self, faction: 'Faction', ordered_ap):
-        if not faction.is_alive:
-            print("“The faction you want to sell armors is dead!")
-            return False
-
-        if ordered_ap > self.ap_left:
-            print("You try to sell more armors than you have in possession.")
-            return False
-
-        self.ap_left -= ordered_ap
-        print("Armors sold!")
-        return True
-
-    def end_turn(self):
-        self.wp_left = Merchant.starting_wp
-        self.ap_left = Merchant.starting_ap
+        return '"You cannot reach our elegance." \n' + super().__str__()
